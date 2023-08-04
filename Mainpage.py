cd92769511036aef1fd07a252869bf1efaadf772
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 14:51:43 +0800

    the program worked after deleting the script on the function page

--- Mainpage.py
+++ Mainpage.py
@@ -2,5 +2,7 @@ from TV_UML import TV
 
 if __name__=='__main__':
         tv1=TV()
-
+        tv1.setChannel(30)
+        tv1.setVolume(3)
+        
 print("tv1's channel is",tv1.getChannel(),"and volume level is",tv1.getVolume())
--- TV_UML.py
+++ TV_UML.py
@@ -49,13 +49,3 @@ class TV:
         else:
             self.channel=7
 
-if __name__=='__main__':
-        tv1=TV()
-        tv2=TV()
-        tv1.setChannel(30)
-        tv1.setVolume(3)
-        tv2.setChannel(3)
-        tv2.setVolume(2)
-
-print("tv1's channel is",tv1.getChannel(),"and volume level is",tv1.getVolume())
-print("tv2's channel is",tv2.getChannel(),"and volume level is",tv2.getVolume())
