d120dcc20dd420e1165094e399dcb13ee053e77b
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 14:29:59 +0800

    pseudocode

---
+++ TV UML.py
@@ -0,0 +1,19 @@
+#Channel (1 to 120)
+#volume (1 to 7)
+
+#Indicate the TV is on/off
+
+#Turns on Tv
+#Turns off Tv
+#return channel
+
+#new channel
+#gets volume
+
+#sets a new volume
+
+#increases channel 
+#descreases channel
+
+#increases volume
+#decreases volume
