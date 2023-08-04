46df07def8c1f2bc9a75a5919d612d5c255feecf
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 14:35:37 +0800

    function for turning on tv

--- TV UML.py
+++ TV UML.py
@@ -3,14 +3,12 @@ class TV:
         self.channel = 1
         self.volumelevel = 1
         self.on = False
-
-
 #Channel (1 to 120)
 #volume (1 to 7)
-
-#Indicate the TV is on/off
-
-#Turns on Tv
+    #Indicate the TV is on/off
+        #Turns on Tv
+    def turnOn(self):
+        self.on=True
 #Turns off Tv
 #return channel
 
